d7fd61461ffb58726d3df51012e3f6334cbdafa6
Berna <maximilianobernardo1@gmail.com>
2022-01-24 08:09:15 -0300

    entrega uno

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+    return (
+    <nav className="navbar-nav">
+        <ul>
+            <li>
+            <a href="index.html">Inicio</a>
+            </li>
+            <li>Galeria</li>
+            <li>Delivery</li>
+            <li>Sabores</li>
+            <li>Contacto</li>
+        </ul>
+    </nav>
+    );
+}
+
+export default NavBar;
